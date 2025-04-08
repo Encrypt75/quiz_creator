21a46eabcb27762b8f80b3710b9f3cf16ca3b85d
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-04-09 00:12:07 +0800

    quiz_creator_progress2
    File handling was introduced, in this program, it used append to transfer any file to a .txt file

--- quiz_creator.py
+++ quiz_creator.py
@@ -10,3 +10,8 @@ option_d = input("Type choice d: ")
 #ask for the correct answer
 correct_ans = input("Correct anwer is: ")
 
+text_file = open("text_file.txt", "a")
+
+text_file.write(f"{question}\n{option_a}\n{option_b}\n{option_c}\n{option_d}\ncorrect answer: {correct_ans}")
+
+text_file.close()
