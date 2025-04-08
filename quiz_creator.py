fc07c4af6be79d30142ecf8eabfaf087c51f8b0c
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-04-08 23:57:29 +0800

    quiz_creator_progress
    Ask the user for their desired input (questions, choices, answers)

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,3 +1,12 @@
 #ask user for their first input
 question = input("Input question:\n> ")
 
+#ask for options
+option_a = input("Type choice a: ")
+option_b = input("Type choice b: ")
+option_c = input("Type choice c: ")
+option_d = input("Type choice d: ")
+
+#ask for the correct answer
+correct_ans = input("Correct anwer is: ")
+
