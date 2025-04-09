1a40f1abd1938098eb45b65ef449028bf6ee3826
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-04-10 00:19:59 +0800

    quiz_creator_progress
    def function and while loop was introduces. This program asks the user for questions, possible answers, and correct answer, then will append to a txt file for all the questions and data to be saved. The loop is for asking the user again and again until the user decides to end and exit from the program.

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,17 +1,28 @@
-#ask user for their first input
-question = input("Input question:\n> ")
+def format(num):
+    #ask user for their first input
+    question = input(f"\nInput question no. {num}: ")
 
-#ask for options
-option_a = input("Type choice a: ")
-option_b = input("Type choice b: ")
-option_c = input("Type choice c: ")
-option_d = input("Type choice d: ")
+    #ask for options
+    option_a = input("Type choice a: ")
+    option_b = input("Type choice b: ")
+    option_c = input("Type choice c: ")
+    option_d = input("Type choice d: ")
 
-#ask for the correct answer
-correct_ans = input("Correct anwer is: ")
+    #ask for the correct answer
+    correct_ans = input("Correct answer is: ")
 
-text_file = open("text_file.txt", "a")
+    text_file = open("text_file.txt", "a")
 
-text_file.write(f"{question}\n{option_a}\n{option_b}\n{option_c}\n{option_d}\ncorrect answer: {correct_ans}")
+    text_file.write(f"{num}. {question}\na. {option_a}\nb. {option_b}\nc. {option_c}\nd. {option_d}\nCorrect Answer: {correct_ans}\n\n")
 
-text_file.close()
+    text_file.close()
+
+    return None
+
+num = 1
+while True:
+    format(num)
+    again = input("Add another question? (yes/no): ").lower()
+    num += 1
+    if again != "yes":
+        break
