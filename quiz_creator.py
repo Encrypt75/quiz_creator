bcc84112d4286455aacd7476b99e7fee9c7e7930
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-04-10 20:02:38 +0800

    quiz_creator_update
    Update: Invalid input are not accepted, using def function

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,8 +1,32 @@
-#import colorama
+#import needed modules
+import random
 from colorama import init, Fore, Style
 
 #initialize colorama
 init()
+
+def valid_input(correct_letter):
+    valid_input_for_choices = ["a", "b", "c", "d"]
+
+    while True:
+        select = input(correct_letter).lower()
+
+        if select in valid_input_for_choices:
+            return select
+        else:
+            print("Invalid input, try again")
+
+#list of messages
+messages = [
+    "Nice question you have there", 
+    "That questions slaps!", 
+    "That questions on fire!", 
+    "You really good at making quiz", 
+    "Whoo..thats a difficult one", 
+    "Even Einstien can't handle that one"
+]
+
+rand_mssg = random.choice(messages)
 
 def format(num):
     #ask user for their question input
@@ -15,40 +39,46 @@ def format(num):
     option_d = input(Fore.YELLOW + "Enter option d: " + Style.RESET_ALL)
 
     #ask for the correct answer
-    correct_ans = input(Fore.GREEN + Style.BRIGHT + "\nEnter the correct answer: " + Style.RESET_ALL).lower()
+    correct_ans = valid_input(Fore.GREEN + Style.BRIGHT + "\nEnter the correct answer: " + Style.RESET_ALL)
 
     text_file = open("text_file.txt", "a")
 
-    text_file.write("\n=============================================")
+    #format in txt file
     text_file.write(f"\n{num}. {question}\n\na. {option_a}\nb. {option_b}\nc. {option_c}\nd. {option_d}\nCorrect Answer: {correct_ans}\n")
     text_file.write("=============================================\n")
 
     text_file.close()
 
+    #conditional statements
     if correct_ans == "a":
         print("\na. ", Fore.GREEN + Style.BRIGHT + option_a + Style.RESET_ALL)
         print("b. ", Fore.RED + option_b + Style.RESET_ALL) 
         print("c. ", Fore.RED + option_c + Style.RESET_ALL)
         print("d. ", Fore.RED + option_d + Style.RESET_ALL)
+        print(f"\n{rand_mssg}")
 
     elif correct_ans == "b":
         print("\na. ", Fore.RED + option_a + Style.RESET_ALL)
         print("b. ", Fore.GREEN + Style.BRIGHT + option_b + Style.RESET_ALL)
         print("c. ", Fore.RED + option_c + Style.RESET_ALL)
         print("d. ", Fore.RED +  option_d + Style.RESET_ALL)
+        print(f"\n{rand_mssg}")
 
     elif correct_ans == "c":
         print("\na. ", Fore.RED + option_a + Style.RESET_ALL)
         print("b. ", Fore.RED + option_b + Style.RESET_ALL)
         print("c. ", Fore.GREEN + Style.BRIGHT + option_c + Style.RESET_ALL)
         print("d. ", Fore.RED +  option_d + Style.RESET_ALL)
+        print(f"\n{rand_mssg}")
 
     else:
         print("\na. ", Fore.RED + option_a + Style.RESET_ALL)
         print("b. ", Fore.RED + option_b + Style.RESET_ALL)
         print("c. ", Fore.RED + option_c + Style.RESET_ALL)
         print("d. ", Fore.GREEN + Style.BRIGHT + option_d + Style.RESET_ALL)
-           
+        print(f"\n{rand_mssg}")
+
+#loop that asks the user for infinite number of times    
 num = 1
 while True:
     format(num)
