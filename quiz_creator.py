0d82bc585d0a5afaa78f92d3207265ea8447f0fa
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-04-10 15:56:09 +0800

    quiz_creator_update
    Changes: This program is now mixed with previous file (the file with import colorama), to the main file. This still works the same but with added effects, like color.

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,28 +1,56 @@
+#import colorama
+from colorama import init, Fore, Style
+
+#initialize colorama
+init()
+
 def format(num):
-    #ask user for their first input
-    question = input(f"\nInput question no. {num}: ")
+    #ask user for their question input
+    question = input(Fore.YELLOW + Style.BRIGHT + f"\nEnter a question no. {num}: " + Style.RESET_ALL)
 
     #ask for options
-    option_a = input("Type choice a: ")
-    option_b = input("Type choice b: ")
-    option_c = input("Type choice c: ")
-    option_d = input("Type choice d: ")
+    option_a = input(Fore.YELLOW + "Enter option a: " + Style.RESET_ALL)
+    option_b = input(Fore.YELLOW + "Enter option b: " + Style.RESET_ALL)
+    option_c = input(Fore.YELLOW + "Enter option c: " + Style.RESET_ALL)
+    option_d = input(Fore.YELLOW + "Enter option d: " + Style.RESET_ALL)
 
     #ask for the correct answer
-    correct_ans = input("Correct answer is: ")
+    correct_ans = input(Fore.GREEN + Style.BRIGHT + "\nEnter the correct answer: " + Style.RESET_ALL).lower()
 
     text_file = open("text_file.txt", "a")
 
-    text_file.write(f"{num}. {question}\na. {option_a}\nb. {option_b}\nc. {option_c}\nd. {option_d}\nCorrect Answer: {correct_ans}\n\n")
+    text_file.write(f"\n{num}. {question}\n\na. {option_a}\nb. {option_b}\nc. {option_c}\nd. {option_d}\nCorrect Answer: {correct_ans}\n")
 
     text_file.close()
 
-    return None
+    if correct_ans == "a":
+        print("\na. ", Fore.GREEN + Style.BRIGHT + option_a + Style.RESET_ALL)
+        print("b. ", Fore.RED + option_b + Style.RESET_ALL) 
+        print("c. ", Fore.RED + option_c + Style.RESET_ALL)
+        print("d. ", Fore.RED + option_d + Style.RESET_ALL)
 
+    elif correct_ans == "b":
+        print("\na. ", Fore.RED + option_a + Style.RESET_ALL)
+        print("b. ", Fore.GREEN + Style.BRIGHT + option_b + Style.RESET_ALL)
+        print("c. ", Fore.RED + option_c + Style.RESET_ALL)
+        print("d. ", Fore.RED +  option_d + Style.RESET_ALL)
+
+    elif correct_ans == "c":
+        print("\na. ", Fore.RED + option_a + Style.RESET_ALL)
+        print("b. ", Fore.RED + option_b + Style.RESET_ALL)
+        print("c. ", Fore.GREEN + Style.BRIGHT + option_c + Style.RESET_ALL)
+        print("d. ", Fore.RED +  option_d + Style.RESET_ALL)
+
+    else:
+        print("\na. ", Fore.RED + option_a + Style.RESET_ALL)
+        print("b. ", Fore.RED + option_b + Style.RESET_ALL)
+        print("c. ", Fore.RED + option_c + Style.RESET_ALL)
+        print("d. ", Fore.GREEN + Style.BRIGHT + option_d + Style.RESET_ALL)
+           
 num = 1
 while True:
     format(num)
-    again = input("Add another question? (yes/no): ").lower()
+    again = input("\nAdd another question? (yes/no): ").lower()
     num += 1
     if again != "yes":
         break
