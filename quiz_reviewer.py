cc0b31059259ee0ed3aecd1fee355642c92c658b
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-05-04 07:48:32 +0800

    quiz_reviewer_two
    dictionary was added to the program for the users input.

--- quiz_reviewer.py
+++ quiz_reviewer.py
@@ -1,4 +1,14 @@
 #ask user for inputs like question, options, and correct answer
-question = input("Enter question: ")
+questions = input("Enter question: ")
 options = [input(f"Enter option {choice}: ") for choice in ["a", "b", "c", "d"]]
-correct_answer = input("Enter correct answer: ")
+correct_answers = input("Enter correct answer: ")
+
+#dictionary for users' input
+data_format = {
+    "question: ": questions,
+    "option: ": options, 
+    "correct answer: ": correct_answers
+}
+#checking if the dictionary works
+for key in data_format:
+    print(key, data_format[key])
