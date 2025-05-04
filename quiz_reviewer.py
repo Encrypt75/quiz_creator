eab70d641a4f65b7456cff8a6fcfb15b482f3315
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-05-04 08:23:29 +0800

    quiz_reviewer_four
    loop was added for the user to input multiple question. Issues: when the user inputs a new question the previous question will be removed

--- quiz_reviewer.py
+++ quiz_reviewer.py
@@ -1,22 +1,38 @@
 import json
 
-#ask user for inputs like question, options, and correct answer
-questions = input("Enter question: ")
-options = [input(f"Enter option {choice}: ") for choice in ["a", "b", "c", "d"]]
-correct_answers = input("Enter correct answer: ")
+def yes_no_input():
+    while True:
+        again = input("Do you want to add another question? (yes/no): ").lower()
+        if again in ["yes", "no"]:
+            return again  # Return the valid input to use outside
+        else:
+            print("Invalid input. Try (yes/no)")
 
-#dictionary for users' input
-data_format = {
-    "question: ": questions,
-    "option: ": options, 
-    "correct answer: ": correct_answers
-}
-#initialize a list
-quiz_data = []
+num = 1
+while True:
+    #ask user for inputs like question, options, and correct answer
+    questions = input(f"\nEnter question {num}: ")
+    options = [input(f"Enter option {choice}: ") for choice in ["a", "b", "c", "d"]]
+    correct_answers = input(f"Enter correct answer: ")
 
-#then appends the input to the list
-quiz_data.append(data_format)
+    #dictionary for users' input
+    data_format = {
+        "question: ": questions,
+        "option: ": options, 
+        "correct answer: ": correct_answers
+    }
+    #initialize a list
+    quiz_data = []
 
-#open a json file to store the data
-with open("json_text.json", "w") as file:
-    json.dump(data_format, file, indent=4)
+    #then appends the input to the list
+    quiz_data.append(data_format)
+
+    #open a json file to store the data
+    with open("json_text.json", "w") as file:
+        json.dump(data_format, file, indent=4)
+
+    #asks the user to input another
+    again = yes_no_input()
+    num += 1
+    if again != "yes":
+        break
