2c678f06c625d540fd35d1185d5b87e0cd5afc2b
Encrypt75 <manaogcarljoshuam@gmail.com>
2025-05-10 01:39:02 +0800

    quiz_reviewer_9
    Finally, bugs was fixed and organize, json file was fixed as well

--- quiz_reviewer.py
+++ quiz_reviewer.py
@@ -1,14 +1,27 @@
 import json
+import os
 
-#initialize a list
-quiz_data = []
+#loads data from a json file
+def file_load():
+    if os.path.exists("json_text.json"):
+        with open("json_text.json", "r") as file:
+            try:
+                quiz_data = json.load(file)
+                if not isinstance(quiz_data, list):
+                    return []
+            except json.JSONDecodeError:
+                    pass
+    return []
+
+#callout file_load()
+quiz_data = file_load()   
 
 #where the loop begins
 while True: 
         #ask user for inputs like question, options, and correct answer
         questions = input(f"\nEnter question: ")
         options = [input(f"Enter option {choice}: ") for choice in ["a", "b", "c", "d"]]
-        correct_answers = input(f"Enter correct answer: ")
+        correct_answers = input(f"Enter correct answer: ").lower()
 
         #creates dictionary for users' input
         data_format = {
@@ -20,11 +33,11 @@ while True:
         #then appends the input to the list
         quiz_data.append(data_format)
 
-        #open a json file to store the data
-        with open("json_text.json", "a") as file:
-            json.dump(data_format, file, indent=4)
-        
         try_again = input("add another question? (y/n): ").lower()
 
-        if try_again == "n":
+        if try_again == "n" or try_again == "no":
             break
+
+#open a json file to store the data
+with open("json_text.json", "w") as file:
+    json.dump(data_format, file, indent=4)
